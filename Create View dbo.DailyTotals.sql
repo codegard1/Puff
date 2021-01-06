74e56b883d65256ddf96bfdddc7347f0d1199660
Chris Odegard <c.odegard@gmail.com>
2021-01-06 12:44:39 -0500

    Update view to replace ad-hoc table in Power BI

--- Create View dbo.DailyTotals.sql
+++ Create View dbo.DailyTotals.sql
@@ -1,18 +1,28 @@
-CREATE VIEW dbo.DailyTotals
+ALTER VIEW dbo.DailyTotals
 AS
+
 SELECT 
       CONVERT(DATE, [Date_Time]) as 'Date'
-    , COUNT([Puff]) as 'Puff Count'
-    , SUM([Time__s_]) as 'Sum Time'
-    , SUM([Energy__mWh_]) as 'Sum Energy'
-    , SUM([Power__W_]) as 'Sum Power'
+    , COUNT([Puff]) as 'puffs'
+    
+    , AVG([Energy__mWh_]) as 'energy_mean'
+    , SUM([Energy__mWh_]) as 'energy_per_day'
+
+    , AVG([Time__s_]) as 'time_mean'
+    , SUM([Time__s_]) as 'time_per_day'
+
+    , AVG([Power__W_]) as 'power_mean'
+    , SUM([Power__W_]) as 'power_per_day'
+
     , SUM([Power_Set__W_]) as 'Sum Power Set'
     , SUM([Cold_Ohms]) as 'Sum Cold Ohms'
     , SUM([Cold_Temp__degF_]) as 'Sum Cold Temp'
-    , SUM([Mod_Resistance]) as 'Sum ModResistance'
-    , SUM([Room_Temp]) as 'Sum Room Temp'
     , SUM([Board_Temp]) as 'Sum Board Temp'
     , SUM([Snapshot_Ohms]) as 'Sum SnapshotOhms'
+
+    -- , SUM([Mod_Resistance]) as 'Sum ModResistance'
+    -- , SUM([Room_Temp]) as 'Sum Room Temp'
   FROM [Puff].[dbo].[Puff]
   GROUP BY CONVERT(DATE, [Date_Time])
---   ORDER BY CONVERT(DATE, [Date_Time]) DESC
+
+GO
